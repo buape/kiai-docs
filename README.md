b1f80a573a8d9e4143c183cc55be0d8b5b382ec6
Shadow <hi@shadowing.dev>
2023-04-05 16:13:57 -0500

    Initial commit from template

---
+++ README.md
@@ -0,0 +1,15 @@
+# Kiai Documentation
+
+This is the public documentation for Kiai, created with [Nextra](https://nextra.site).
+
+[**View Site**](https://docs.kiaibot.com)
+
+## Local Development
+
+First, run `pnpm i` to install the dependencies.
+
+Then, run `pnpm dev` to start the development server and visit localhost:3000.
+
+## License
+
+This project is licensed under the MIT License.
---
+++ theme.config.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { DocsThemeConfig } from 'nextra-theme-docs'
+
+const config: DocsThemeConfig = {
+  logo: <span>Kiai Docs</span>,
+  project: {
+    link: 'https://github.com/buape/kiai-docs',
+  },
+  chat: {
+    link: 'https://discord.gg/T6gwuyNSv3',
+  },
+  docsRepositoryBase: 'https://github.com/buape/kiai-docs/tree/main',
+  footer: {
+    text: "© 2023 Buape Studios"
+  }
+}
+
+export default config
