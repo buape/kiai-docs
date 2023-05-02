ac4d6fa4ea1991004d2d827ab824667f1f198846
Shadow <hi@shadowing.dev>
2023-05-02 12:18:36 -0500

    Forgot to push

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,18 +1,18 @@
-import React from 'react'
-import { DocsThemeConfig } from 'nextra-theme-docs'
+import React from "react";
+import { DocsThemeConfig } from "nextra-theme-docs";
 
 const config: DocsThemeConfig = {
-  logo: <span>Kiai Docs</span>,
-  project: {
-    link: 'https://github.com/buape/kiai-docs',
-  },
-  chat: {
-    link: 'https://discord.gg/T6gwuyNSv3',
-  },
-  docsRepositoryBase: 'https://github.com/buape/kiai-docs/tree/main',
-  footer: {
-    text: "© 2023 Buape Studios"
-  }
-}
+	logo: <span>Kiai Documentation</span>,
+	project: {
+		link: "https://github.com/buape/kiai-docs",
+	},
+	chat: {
+		link: "https://discord.gg/T6gwuyNSv3",
+	},
+	docsRepositoryBase: "https://github.com/buape/kiai-docs/tree/main",
+	footer: {
+		text: "© 2023 Buape Studios",
+	},
+};
 
-export default config
+export default config;
