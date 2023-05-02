d732410e712736027381ce31a67c52681b2008e8
Shadow <hi@shadowing.dev>
2023-05-02 17:28:08 -0500

    Add more meta tweaks

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { DocsThemeConfig } from "nextra-theme-docs";
+import { DocsThemeConfig, useConfig } from "nextra-theme-docs";
+import { useRouter } from "next/router";
 
 const config: DocsThemeConfig = {
 	logo: <span>Kiai Documentation</span>,
@@ -13,6 +14,40 @@ const config: DocsThemeConfig = {
 	footer: {
 		text: "© 2023 Buape Studios",
 	},
+	useNextSeoProps() {
+		const { asPath } = useRouter();
+		if (asPath !== "/") {
+			return {
+				titleTemplate: "%s – Kiai Docs",
+			};
+		}
+	},
+	head: () => {
+		const { asPath, defaultLocale, locale } = useRouter();
+		const { frontMatter } = useConfig();
+		const url =
+			"https://docs.kiaibot.com" +
+			(defaultLocale === locale ? asPath : `/${locale}${asPath}`);
+
+		return (
+			<>
+				<meta property="og:url" content={url} />
+				<meta
+					property="og:title"
+					content={frontMatter.title || "Kiai Documentation"}
+				/>
+				<meta
+					property="og:description"
+					content={
+						frontMatter.description ||
+						"The main documentation for Kiai"
+					}
+				/>
+			</>
+		);
+	},
+	logoLink: "https://raw.githubusercontent.com/buape/resources/main/kiai.png",
+	
 };
 
 export default config;
