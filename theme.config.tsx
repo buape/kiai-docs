6fdd2ef54d42e1bbce06863713ee9631bb8b84ea
Shadow <shadow@buape.com>
2023-05-02 19:16:55 -0500

    Initial v1 docs (#15)
    * Initial v1 docs
    
    * more docs
    
    * update packages
    
    * Forgot to push
    
    * Apply suggestions from code review
    
    Co-authored-by: net-tech- <hello@nettech.dev>
    
    * Add more meta tweaks
    
    * change navbar title
    
    ---------
    
    Co-authored-by: net-tech- <hello@nettech.dev>

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,18 +1,53 @@
-import React from 'react'
-import { DocsThemeConfig } from 'nextra-theme-docs'
+import React from "react";
+import { DocsThemeConfig, useConfig } from "nextra-theme-docs";
+import { useRouter } from "next/router";
 
 const config: DocsThemeConfig = {
-  logo: <span>Kiai Docs</span>,
-  project: {
-    link: 'https://github.com/buape/kiai-docs',
-  },
-  chat: {
-    link: 'https://discord.gg/T6gwuyNSv3',
-  },
-  docsRepositoryBase: 'https://github.com/buape/kiai-docs/tree/main',
-  footer: {
-    text: "© 2023 Buape Studios"
-  }
-}
+	logo: <span>Kiai Documentation</span>,
+	project: {
+		link: "https://github.com/buape/kiai-docs",
+	},
+	chat: {
+		link: "https://discord.gg/T6gwuyNSv3",
+	},
+	docsRepositoryBase: "https://github.com/buape/kiai-docs/tree/main",
+	footer: {
+		text: "© 2023 Buape Studios",
+	},
+	useNextSeoProps() {
+		const { asPath } = useRouter();
+		if (asPath !== "/") {
+			return {
+				titleTemplate: "%s – Kiai Docs",
+			};
+		}
+	},
+	head: () => {
+		const { asPath, defaultLocale, locale } = useRouter();
+		const { frontMatter } = useConfig();
+		const url =
+			"https://docs.kiaibot.com" +
+			(defaultLocale === locale ? asPath : `/${locale}${asPath}`);
 
-export default config
+		return (
+			<>
+				<meta property="og:url" content={url} />
+				<meta
+					property="og:title"
+					content={frontMatter.title || "Kiai Documentation"}
+				/>
+				<meta
+					property="og:description"
+					content={
+						frontMatter.description ||
+						"The main documentation for Kiai"
+					}
+				/>
+			</>
+		);
+	},
+	logoLink: "https://raw.githubusercontent.com/buape/resources/main/kiai.png",
+	
+};
+
+export default config;
